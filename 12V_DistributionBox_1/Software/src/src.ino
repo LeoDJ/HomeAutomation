68de112fc2c2997ed38019d030091749bcf7ed67
Leandro <leandro.spaeth@gmail.com>
2017-10-06 02:18:41 +0200

    added button handling especially handling dimming over a single button

--- 12V_DistributionBox_1/Software/src/src.ino
+++ 12V_DistributionBox_1/Software/src/src.ino
@@ -32,9 +32,17 @@
 #define MY_RADIO_NRF24
 //#define MY_RADIO_RFM69
 
+#define MY_RF24_CE_PIN 7
+#define MY_RF24_CS_PIN 8
+
+#define MY_TRANSPORT_WAIT_READY_MS 5000
+#define MY_TRANSPORT_SANITY_CHECK
+
 #include <MySensors.h>
 
 #define FADE_DELAY 10  // Delay in ms for each percentage fade up/down (10ms = 1s full-range dim)
+const byte dimmerButtonDivisor = 20; //do one fade step every x ms
+const int  dimmerDimDelay = 200; //time in ms to wait before dimming sets in, before that it simply switches
 
 const byte voltageMeasurePin = A7;
 const float voltsPerADCStep = 0.0146;
@@ -46,10 +54,11 @@ const byte buttons[] = {A3, A0, A2, A1}; //maps to all dimmers then to all outpu
 const byte dimmerChannelOffset = 4; //move digital pins behind in the channel mapping (for expandability)
 const byte powerChannelOffset = 8; 
 
-byte dimmerLevel[sizeof(dimmerOutputs)], targetDimValue[sizeof(dimmerOutputs)];
-bool fadeRunning[sizeof(dimmerOutputs)];
-bool outputState[sizeof(powerOutputs)];
-bool lastBtnState[sizeof(buttons)];
+const byte dimCount = sizeof(dimmerOutputs), outCount = sizeof(powerOutputs);
+
+byte dimmerLevel[dimCount], targetDimValue[dimCount], lastDimLevel[dimCount];
+bool fadeRunning[dimCount];
+bool outputState[outCount];
 
 byte debounceTime = 40;
 
@@ -57,16 +66,16 @@ void setup()
 {
     for(byte i = 0; i < sizeof(buttons); i++) {
         pinMode(buttons[i], INPUT_PULLUP);
-        lastBtnState[i] = 1;
     }
-    for(byte i = 0; i < sizeof(powerOutputs); i++) { //sizeof reports size in bytes
+    for(byte i = 0; i < outCount; i++) { //sizeof reports size in bytes
         //outputState[i] = request(i + powerChannelOffset, V_STATUS); //currently does not work
         pinMode(powerOutputs[i], OUTPUT);
         setOutput(i, 0);
     }
-    for(byte i = 0; i < sizeof(dimmerOutputs); i++) { //sizeof reports size in bytes
+    for(byte i = 0; i < dimCount; i++) { //sizeof reports size in bytes
         //byte level = request(i + dimmerChannelOffset, V_PERCENTAGE); //currently does not work
         pinMode(dimmerOutputs[i], OUTPUT);
+        lastDimLevel[i] = 100;
         fadeToLevel(i, /*level*/0);
     }
 }
@@ -75,8 +84,10 @@ void presentation()
 {
     // Register the LED Dimmable Light with the gateway
     present(0, S_MULTIMETER);
-    present(4, S_DIMMER);
-    for(byte i = 0; i < sizeof(powerOutputs); i++) { //sizeof reports size in bytes
+    for(byte i = 0; i < dimCount; i++) { //sizeof reports size in bytes
+        present(i + dimmerChannelOffset, S_DIMMER);
+    }
+    for(byte i = 0; i < outCount; i++) { //sizeof reports size in bytes
         present(i + powerChannelOffset, S_BINARY);
     }
     
@@ -96,17 +107,68 @@ void loop()
 }
 
 unsigned long lastBtnPress = 0;
+char dimmerDelta[dimCount] = {0};
+unsigned long buttonPressedAt[sizeof(buttons)] = {0};
 
-void handleButtons() { //TODO: Dimmer button, no head for it right now :P
+byte getTimeDiffDimmerValue(byte id) {
+    byte val = constrain((int)dimmerLevel[id] + (int)(dimmerDelta[id] * ((millis() - buttonPressedAt[id] - dimmerDimDelay) / dimmerButtonDivisor)), 0, 100); //hopefully doesn't overflow
+    //Serial.println("dimlev: " + String(dimmerLevel[id]) + " | dimDelt: " + String((int)dimmerDelta[id]) + " | calcDetail: " + String((dimmerDelta[id] * (millis() - buttonPressedAt[id]) / dimmerButtonDivisor)) + " | val: " + String(val));
+    return val;
+}
+
+void handleButtons() {
     for(byte i = 0; i < sizeof(buttons); i++) {
         bool state = digitalRead(buttons[i]);
-        if(state != lastBtnState[i]) {
-            lastBtnState[i] = state;
-            if(millis() - lastBtnPress > debounceTime) {
-                if(!state) {
-                    toggleOutput(i - sizeof(dimmerOutputs)); //button mapping
+        bool isDigital = i >= dimCount;
+        if(!state && buttonPressedAt[i] == 0) { //button rising edge
+            buttonPressedAt[i] = millis(); //save when button was pressed
+            Serial.println(String(millis()) + " Button B" + String(i) + " rising edge detected");
+        }
+        else if(!state && buttonPressedAt[i] != 0) { //button is being held
+            if(!isDigital) { //handling for dimmer buttons | TODO: can be placed in if clause above
+                if(millis() - buttonPressedAt[i] > dimmerDimDelay) { //if in dimming range of button press
+                    if(dimmerLevel[i] == 0) //set dimming direction, if at end of range
+                        dimmerDelta[i] = 1;
+                    else if(dimmerLevel[i] == 100) 
+                        dimmerDelta[i] = -1;
+                    setBrightness(i, getTimeDiffDimmerValue(i));
+                    //Serial.println(String(millis()) + " Button B" + String(i) + " currently dimming... " + String(getTimeDiffDimmerValue(i)));
                 }
             }
+        }
+        else if(state && buttonPressedAt[i] != 0) { //if button released
+            Serial.println(String(millis()) + " Button B" + String(i) + " released. Was pressed at " + String(buttonPressedAt[i]));
+            if(!isDigital) {
+                byte newLevel;
+                if(millis() - buttonPressedAt[i] > dimmerDimDelay) { //if it had been dimming 
+                    dimmerLevel[i] = getTimeDiffDimmerValue(i);
+                    dimmerDelta[i] = -dimmerDelta[i];
+                    if(dimmerLevel[i] > 0) //workaround for the button push to correctly turn off LED
+                        lastDimLevel[i] = 0;
+                    newLevel = dimmerLevel[i];
+                }
+                else { //if dimmer button was only shortly pushed
+                    fadeToLevel(i, lastDimLevel[i]);
+                    newLevel = lastDimLevel[i];
+                    if(lastDimLevel[i] == 0)
+                        lastDimLevel[i] = dimmerLevel[i];
+                    else 
+                        lastDimLevel[i] = 0;
+                    //Serial.println(String(millis()) + " Button B" + String(i) + " released. New dimLev: " + String(dimmerLevel[i]) + " | lastLev: " + String(lastDimLevel[i]));
+                }
+                buttonPressedAt[i] = 0;
+                MyMessage dim(i + dimmerChannelOffset, V_DIMMER); //hopefully won't cause an overflow
+                MyMessage stat(i + dimmerChannelOffset, V_STATUS);
+                send(stat.set(newLevel > 0));
+                send(dim.set(newLevel));
+                
+            }
+            else if(isDigital && millis() - buttonPressedAt[i] >= debounceTime) {
+                toggleOutput(i - dimCount);
+                Serial.println(String(millis()) + " Button B" + String(i) + " toggled digital output " + String(buttonPressedAt[i]));
+                buttonPressedAt[i] = 0;
+            }
+
         }
     }
 }
@@ -209,7 +271,7 @@ void setBrightness(byte dimmerId, byte level) { //level: 0-100
 }
 
 void fadeTick() {
-    for(byte i = 0; i < sizeof(dimmerOutputs); i++) {
+    for(byte i = 0; i < dimCount; i++) {
         if(fadeRunning[i]) {
             byte target = targetDimValue[i];
             byte level = dimmerLevel[i];
