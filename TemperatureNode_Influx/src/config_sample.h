091268636a486913cc81e109613ae3d158c2cc20
Leandro Späth <leandro.spaeth@gmail.com>
2018-03-20 21:17:47 +0100

    add sample config

--- TemperatureNode_Influx/src/config_sample.h
+++ TemperatureNode_Influx/src/config_sample.h
@@ -1,1 +1,12 @@
 // ATTENTION: this is a sample config file. Replace placeholder values with correct ones and rename file to "config.h"
+
+const char* ssid = "xxx";
+const char* pass = "xxx";
+
+const char* otaPassword = "xxx"; //leave empty for no password
+
+const char* influxHost = "xxx";                 //IP address or hostname
+const int influxPort = 8086;                    //default port
+const String database = "xxx";
+const String measurement = "xxx";               //measurement to push the data to
+const String auth = "dXNlcm5hbWU6cGFzc3dvcmQ="; //simple base64 encoded string username:password, leave empty for no authentication
